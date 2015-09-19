6b4a3606c8ba94fe8f01face6dcd856cf00a7339
RachelD <rachel.m.dorn@gmail.com>
2015-09-19 14:31:06 -0400

    add doors

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ $('button').click(function(){
 	var room = new Room();
 	console.log(room.doors);
 	drawGrid();
+	drawDoors(room);
 });
 
 function drawGrid() {
@@ -15,5 +16,21 @@ function drawGrid() {
 		for (j = 1; j < 11; j++){
 			ctx.strokeRect(50*j, 50*i, 50, 50);
 		}
+	}
+}
+
+function drawDoors(room) {
+	doors = room.doors;
+	if (doors >= 1){
+		ctx.fillRect(250, 50, 100, 50);
+	}
+	if (doors >= 2){
+		ctx.fillRect(50, 250, 50, 100);
+	}
+	if (doors >= 3){
+		ctx.fillRect(500, 250, 50, 100);
+	}
+	if (doors >= 4){
+		ctx.fillRect(250, 500, 100, 50);
 	}
 }
