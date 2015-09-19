e21ed07a6f0aca590e8f821829c063b932bc3fd5
RachelD <rachel.m.dorn@gmail.com>
2015-09-19 18:01:32 -0400

    change canvas size

--- main.js
+++ main.js
@@ -5,15 +5,20 @@ var horiz = 50;
 var vert = 50;
 
 $('button').click(function(){
+	ctx.clearRect(0, 0, c.width, c.height);
 	var room = new Room();
 	console.log(room.doors);
 	drawGrid();
 	drawDoors(room);
 });
 
+$('#north').click(function(){
+
+});
+
 function drawGrid() {
-	for (i = 1; i < 11; i++){
-		for (j = 1; j < 11; j++){
+	for (i = 0; i < 10; i++){
+		for (j = 0; j < 10; j++){
 			ctx.strokeRect(50*j, 50*i, 50, 50);
 		}
 	}
@@ -22,15 +27,35 @@ function drawGrid() {
 function drawDoors(room) {
 	doors = room.doors;
 	if (doors >= 1){
-		ctx.fillRect(250, 50, 100, 50);
+		ctx.fillRect(200, 0, 100, 50);
 	}
 	if (doors >= 2){
-		ctx.fillRect(50, 250, 50, 100);
+		ctx.fillRect(0, 200, 50, 100);
 	}
 	if (doors >= 3){
-		ctx.fillRect(500, 250, 50, 100);
+		ctx.fillRect(450, 200, 50, 100);
 	}
 	if (doors >= 4){
-		ctx.fillRect(250, 500, 100, 50);
+		ctx.fillRect(200, 450, 100, 50);
+	}
+}
+
+c.addEventListener("mousedown", getPosition, false);
+
+function getPosition(event){
+	var x = event.x - c.offsetLeft;
+	var y = event.y - c.offsetTop;
+	console.log(x + " " + y);
+	if(x >= 500 && x <= 550 && y >= 250 && y <= 350){
+		console.log('right button clicked');
+	}
+	if(x >= 250 && x <= 350 && y >= 50 && y <= 100){
+		console.log('top button clicked');
+	}
+	if(x >= 50 && x <= 100 && y >= 250 && y <= 350){
+		console.log('left button clicked');
+	}
+	if(x >= 250 && x <= 350 && y >= 425 && y <= 475){
+		console.log('bottom button clicked');
 	}
 }
