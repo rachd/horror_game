7dee4953059b7c077e1e6c0a4569f136b5f69785
RachelD <rachel.m.dorn@gmail.com>
2015-09-26 15:59:48 -0400

    add countdown timer

--- main.js
+++ main.js
@@ -136,6 +136,55 @@ $('#east').click(function() {
 	$('.room').text('Current Room: ' + room_num);
 });
 
+/*
+=================
+Countdown Clock
+=================
+*/
+var timeInMinutes = 60;
+var currentTime = Date.parse(new Date());
+var deadline = new Date(currentTime + timeInMinutes*60*1000);
+
+function getTimeRemaining(endtime){
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor( (t/1000) % 60 );
+  var minutes = Math.floor( (t/1000/60) % 60 );
+  var hours = Math.floor( (t/(1000*60*60)) % 24 );
+  var days = Math.floor( t/(1000*60*60*24) );
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+
+function initializeClock(id, endtime){
+	var clock = document.getElementById(id);
+	var hoursSpan = clock.querySelector('.hours');
+	var minutesSpan = clock.querySelector('.minutes');
+	var secondsSpan = clock.querySelector('.seconds');
+    function updateClock(){
+    var t = getTimeRemaining(endtime);
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+    if(t.total<=0){
+      clearInterval(timeinterval);
+    }
+}
+
+updateClock(); // run function once at first to avoid delay
+var timeinterval = setInterval(updateClock,1000);
+}
+
+initializeClock('clockdiv', deadline);
+/*
+===================
+Create First Room
+===================
+*/
 room = new Room();
 rooms.push(room);
 room.creature = Portal;
